37dadd1738761be427a3a81b072b765fdd8323f0
jonalobo <jonathan_1920@hotmail.com>
2023-04-05 19:12:32 -0600

    Casi listo

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express')
 require('dotenv').config()
 
 const PORT = process.env.PORT
+
 const app = express()
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 app.get('/',(req,res)=>{
     res.send('hola')
