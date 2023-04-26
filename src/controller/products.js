206913138ffa89dd26dbd257458249ea2d27560d
jonalobo <jonathan_1920@hotmail.com>
2023-04-26 13:32:36 -0600

    Listo la consigna 3 completo el servidor para pruebas en ruta products y products dinámica

--- src/controller/products.js
+++ src/controller/products.js
@@ -14,25 +14,48 @@ const getProducts = async (req = request, res = response) => {
   //Los paso a Json
   const dataJson = JSON.parse(data);
 
+  //Comprobación si viene el query
   if (limite != undefined) {
     //Debo parsearlo a entero
     const limiteParseado = parseInt(limite);
-    const lista = []
-    for (let i = 0; i < limite; i++) {
+    const lista = [];
+    for (let i = 0; i < limiteParseado; i++) {
       const element = dataJson[i];
-      lista.push(element)
+      lista.push(element);
     }
-    res.json(lista)
+    res.json(lista);
   } else {
     res.json(dataJson);
   }
-
-  
 };
 
 //Controlador de producto por id en parametro valor de ruta dinámico
-const getProductsById = (req = request, res = response) => {
-  res.send("hola");
+const getProductsById = async (req = request, res = response) => {
+  const id = req.params.id;
+  //Voy a parsear el id
+  const idParseado = parseInt(id);
+
+  //Ahora voy a leer los productos almacenados
+  const data = await leer("productos.txt");
+  //Los paso a Json
+  const dataJson = JSON.parse(data);
+
+  const elemento = [];
+
+  dataJson.map((e) => {
+    if (idParseado == e.id) {
+      const demo = e;
+      elemento.push(demo)
+    }
+  });
+
+  if (elemento.length >= 1) {
+    res.json(elemento[0]);
+  } else {
+    res.json({ msg: `El producto con el id ${idParseado} no existe` });
+  }
+
+  
 };
 
 module.exports = {
